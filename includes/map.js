f75e4a1fd3ad987c1f44c6ad4bc46466fbf489e6
Gina Lolli <ginalolli@gmail.com>
2017-02-25 12:51:58 +0100

    display city info, add index

--- includes/map.js
+++ includes/map.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-  
+
   //Show and hide dots based on selected option
   $("#map-form").change(function() {
     var selectedContinent = $("#map-form option:selected").val();
@@ -11,6 +11,21 @@ $(document).ready(function() {
       $('.dot[continent*='+selectedContinent+']').show(1000);
       $('.dot[continent!='+selectedContinent+']').hide(1000);
     };
+  });
+  
+  $('.dot').click(function() {
+    //select the cities by using the selected class
+    $('.dot').removeClass('selected');
+    $(this).addClass('selected');
+
+    //store the city attr in a var
+    var city = $('.city-detail#'+$(this).attr('city'));
+    var htmlCode = $(city).html();
+
+    $('.detail-container').fadeOut(500, function() {
+      $('.detail-container .city-detail').html(htmlCode);
+      $(this).fadeIn(500);
+    })
   });
 
 });
